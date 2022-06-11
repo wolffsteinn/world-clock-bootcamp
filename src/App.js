833a71615c5add5cc729569e136e96a39402f377
neokaiyuan <kai@rocketacademy.co>
2022-06-11 12:03:31 +0800

    Refactor clock display logic into its own component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "./Clock.js";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    // Initialise component state to contain "date" attribute with current date and time
-    this.state = { date: new Date() };
-  }
-
-  componentDidMount() {
-    // Set date value in state every second to current date and time
-    // Save setInterval timer ID in class variable for teardown in another class method
-    this.timerId = setInterval(() => {
-      this.setState({
-        date: new Date(),
-      });
-    }, 1000);
-  }
-
-  componentWillUnmount() {
-    // Teardown setInterval timer with timer ID saved as class variable
-    clearInterval(this.timerId);
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          {/* Render date value that is stored in state */}
-          <p>{this.state.date.toString()}</p>
+          <Clock />
         </header>
       </div>
     );
---
+++ src/Clock.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    // Initialise component state to contain "date" attribute with current date and time
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    // Set date value in state every second to current date and time
+    // Save setInterval timer ID in class variable for teardown in another class method
+    this.timerId = setInterval(() => {
+      this.setState({
+        date: new Date(),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    // Teardown setInterval timer with timer ID saved as class variable
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    return (
+      // Render date value that is stored in state
+      <p>{this.state.date.toString()}</p>
+    );
+  }
+}
+
+export default Clock;
